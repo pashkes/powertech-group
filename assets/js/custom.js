cf40915063461c4f049da1cfedc08ad6a56860ce
pashkes <pashkes@gmail.com>
2017-04-17 21:28:56 +0300

    Update 2017-04-17T18:28:53.204Z

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,52 +4,82 @@
  * Document ready functions
  */
 (function ($) {
-    $(document).ready(function () {
+  $(document).ready(function () {
 
-        //select lang site
-        var lang = $('.header__lang');
-
-        lang.on('click', function() {
-            $(this).toggleClass('header__lang--open');
-        });
-
-        //slider reviews
-        var reviews = $('.slider');
-
-        if(reviews.length) {
-            reviews.slick({
-                infinite: true,
-                speed: 800,
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              responsive: [
-                {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
-            });
-        }
-
-      //Toggle mobile menu
-      var burger = $('.toggle-menu');
-      var header = $('.header');
-
-      burger.on('click', function () {
-        $(this).toggleClass('active');
-        header.toggleClass('js-menu-show');
+    //Initialization custom calendar flatpickr
+    if ($('.technique__field--date').length) {
+      Flatpickr.localize(Flatpickr.l10ns.ru);
+      $(".technique__field--date").flatpickr({
+        disableMobile: true,
+        dateFormat: "d.m.Y"
       });
+    }
 
-      $(window).on('resize', function () {
-        if ($(window).width() > 1024) {
-          header.removeClass('js-menu-show');
-        }
+    //Slider product item
+    if($('.technique').length) {
+      $('.technique__single').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.technique__preview'
       });
+      $('.technique__preview').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: 0,
+        asNavFor: '.technique__single',
+        centerMode: true,
+        focusOnSelect: true,
+        prevArrow: '<button class="technique__arrow technique__arrow--back">назад</button>',
+        nextArrow: '<button class="technique__arrow technique__arrow--next">вперед</button>',
+        arrows: true
+      });
+    }
 
+    //select lang site
+    var lang = $('.header__lang');
+
+    lang.on('click', function () {
+      $(this).toggleClass('header__lang--open');
     });
+
+    //slider reviews
+    var reviews = $('.slider');
+
+    if (reviews.length) {
+      reviews.slick({
+        infinite: true,
+        speed: 800,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    }
+
+    //Toggle mobile menu
+    var burger = $('.toggle-menu');
+    var header = $('.header');
+
+    burger.on('click', function () {
+      $(this).toggleClass('active');
+      header.toggleClass('js-menu-show');
+    });
+
+    $(window).on('resize', function () {
+      if ($(window).width() > 1024) {
+        header.removeClass('js-menu-show');
+      }
+    });
+
+  });
 })(jQuery);
 
 $(function () {
@@ -67,36 +97,36 @@ var TABLET_WIDTH = 768;
 function topImg() {
   if (categories.length) {
     var windowWidth = $(window).width();
-      var categoriesHeight = categoriesInner.innerHeight();
-      var mainImgHeight = mainImg.innerHeight();
-      var categoriesItemHeight = categoriesItem.innerHeight() / 2.35;
+    var categoriesHeight = categoriesInner.innerHeight();
+    var mainImgHeight = mainImg.innerHeight();
+    var categoriesItemHeight = categoriesItem.innerHeight() / 2.35;
 
-      categories.css('margin-top', mainImgHeight);
-      categories.css('height', categoriesHeight - categoriesItemHeight);
-      categoriesInner.css('top', - categoriesItemHeight);
+    categories.css('margin-top', mainImgHeight);
+    categories.css('height', categoriesHeight - categoriesItemHeight);
+    categoriesInner.css('top', -categoriesItemHeight);
 
-      if (windowWidth <= TABLET_WIDTH) {
-        categories.css('height', categoriesHeight);
-      }
+    if (windowWidth <= TABLET_WIDTH) {
+      categories.css('height', categoriesHeight);
+    }
   }
 }
 
 
 $(window).on('load scroll resize ready ontouchstart ontouchmove touchmove', function () {
   topImg();
-  });
+});
 
 //opacity on scroll
 var imgOpacity = $('.header__bg');
 
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
   var scrollCoef = 0.0010;
   imgOpacity.css({
     opacity: .9 - $(window).scrollTop() * scrollCoef
   })
 });
 
-$.fn.inView = function(){
+$.fn.inView = function () {
   //Window Object
   var win = $(window);
   //Object to Check
@@ -107,7 +137,7 @@ $.fn.inView = function(){
   var visibleArea = win.scrollTop() + win.height();
   //the end of the object to check
   var objEndPos = (obj.offset().top + obj.outerHeight());
-  return(visibleArea >= objEndPos && scrollPosition <= objEndPos ? true : false)
+  return (visibleArea >= objEndPos && scrollPosition <= objEndPos ? true : false)
 };
 var isMobile = navigator.userAgent.match(/Mobile/i) == "Mobile";
 
@@ -118,10 +148,10 @@ var isMobile = navigator.userAgent.match(/Mobile/i) == "Mobile";
     var animateElements = [
           $('h1'),
           $('.intro__subtitle'),
-        $('.about__text'),
-        $('.about__more'),
-        $('.about__photo'),
-        $('.features__rhombus')
+          $('.about__text'),
+          $('.about__more'),
+          $('.about__photo'),
+          $('.features__rhombus')
         ],
         visibleClass = "show-element";
 
